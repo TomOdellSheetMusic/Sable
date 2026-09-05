aa4358f760c1bdaf54b17e3d8912ff02505b9526
TomOdellSheetMusic <entey.community@gmail.com>
2026-09-05 17:14:51 +0200

    always navigate to new room even when its a voice room

--- src/app/features/room-nav/RoomNavItem.tsx
+++ src/app/features/room-nav/RoomNavItem.tsx
@@ -387,6 +387,7 @@ export function RoomNavItem({
             room,
             new CallControlState(callPref.microphone, callPref.video, callPref.sound)
           );
+          navigateRoom(room.roomId);
         } else {
           navigateRoom(room.roomId);
         }
